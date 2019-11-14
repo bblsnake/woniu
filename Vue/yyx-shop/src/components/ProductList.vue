239995547df786719b48c35cbaf305facd5eec09
bblsnake <2925141441@qq.com>
2019-11-14 11:16:05 +0800

    vue

---
+++ Vue/yyx-shop/src/components/ProductList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <li v-for="product in products" :key="product.key">
+      {{product.title}}-{{product.price}}
+      <button @click="addProductToCart(product)">add cart</button>
+    </li>
+    
+  </div>
+</template>
+
+<script>
+import { mapState, mapActions } from 'vuex'
+export default {
+  computed: mapState({
+    products: state => state.products.all
+  }),
+  created() {
+    this.$store.dispatch('products/getAllProducts')
+  },
+  methods: mapActions('cart', [
+    'addProductToCart'
+  ]),
+}
+</script>
+
+<style scoped>
+
+</style>
