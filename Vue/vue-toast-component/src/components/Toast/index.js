d0996eee6861094e7b3b4005743d5bd5c38b6e17
bblsnake <2925141441@qq.com>
2019-11-15 10:49:17 +0800

    vue

---
+++ Vue/vue-toast-component/src/components/Toast/index.js
@@ -0,0 +1,20 @@
+import vue from 'vue';
+import ToastComponent from './index.vue';
+// console.log(ToastComponent);
+
+const toastConstruct = vue.extend(ToastComponent);
+function showMessage (msg, duration = 2000) {
+  const instance = new toastConstruct({
+    el: document.createElement('div'),
+    propsData: {
+      msg: msg,
+      visiable: true
+    }
+  })
+  document.body.appendChild(instance.$el);
+  setTimeout(() => {
+    instance.visiable = false
+  }, duration)
+}
+export default showMessage;
+// export default ToastComponent;
